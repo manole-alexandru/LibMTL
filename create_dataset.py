c42b14925b99e03ca08689ce5e16585b9c8f8130
Teodora <teodora.toader0224@yahoo.com>
2024-07-04 20:52:31 +0300

    use pickled festures

--- create_dataset.py
+++ create_dataset.py
@@ -21,22 +21,23 @@ import torch
 
 import torchvision.transforms as transforms
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Ignore warnings
 import warnings
+
 warnings.filterwarnings("ignore")
 
-plt.ion()   # interactive mode
+plt.ion()  # interactive mode
 
 transform = transforms.Compose([transforms.Pad((0, 85), fill=0, padding_mode='constant'),
                                 transforms.Resize((224, 224)),
                                 transforms.ToTensor(),
-                                transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
+                                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
                                 ])
 
 QVQA_PATH = "/content/drive/MyDrive/OVQA_publish/"
+FEATURES_PATH = "/content/drive/MyDrive/OVQA_publish/features.pkl"
 
 
 def compute_prerequisites():
@@ -47,7 +48,7 @@ def compute_prerequisites():
     with open(QVQA_PATH + "valset.json") as f_val:
         data_val = json.load(f_val)
 
-    with open(QVQA_PATH+ "trainset.json") as f_train:
+    with open(QVQA_PATH + "trainset.json") as f_train:
         data_train = json.load(f_train)
 
     with open(QVQA_PATH + "testset.json") as f_test:
@@ -95,11 +96,12 @@ class OVQADataset(Dataset):
                 on a sample.
         """
         with open(json_file) as f:
-          self.question_data = json.load(f)
+            self.question_data = json.load(f)
         self.root_dir = root_dir
         self.transform = transform
         self.task = task
         self.phase = phase
+        self.img_feat_vqa = np.load(FEATURES_PATH, allow_pickle=True)
 
     def __len__(self):
         return len(self.question_data)
@@ -108,30 +110,30 @@ class OVQADataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
+        img_feat_vqa = self.img_feat_vqa
         img_name = os.path.join(self.root_dir,
                                 self.question_data[idx]["image_name"])
 
-        image = Image.open(img_name).convert('RGB')
-        if self.transform:
-          image = self.transform(image)
-          #print(image.shape)
+        image_feat = torch.Tensor(img_feat_vqa[img_name.split('/')[-1]])
 
         question = self.question_data[idx]["question"]
-        answer =  self.question_data[idx]["answer"]
+        answer = self.question_data[idx]["answer"]
         question_type = self.question_data[idx]["question_type"]
-        answer_type =  self.question_data[idx]["answer_type"]
+        answer_type = self.question_data[idx]["answer_type"]
         image_organ = self.question_data[idx]["image_organ"]
         qid = self.question_data[idx]["qid"]
 
-        #print(image.size())
-        sample = {'image': vgg19_model(image[None,...].to(device)),
-            'question': question,
-            'answer_label':F.one_hot(torch.tensor([[ANS_LABLE_DICT[answer]]]), len(ANS_LABLE_DICT)),
-            'question_type_label':F.one_hot(torch.tensor([[Q_TYPE_LABLE_DICT[question_type]]]), len(Q_TYPE_LABLE_DICT)),
-            'answer_text': answer,
-            'answer_type_label':F.one_hot(torch.tensor([[ANS_TYPE_LABLE_DICT[answer_type]]]), len(ANS_TYPE_LABLE_DICT)),
-            'qid': qid}
-            #'image_organ_label': F.one_hot(torch.tensor([[IMG_ORGAN_LABLE_DICT[image_organ]]]), len(IMG_ORGAN_LABLE_DICT))}
+        # print(image.size())
+        sample = {'image': image_feat,
+                  'question': question,
+                  'answer_label': F.one_hot(torch.tensor([[ANS_LABLE_DICT[answer]]]), len(ANS_LABLE_DICT)),
+                  'question_type_label': F.one_hot(torch.tensor([[Q_TYPE_LABLE_DICT[question_type]]]),
+                                                   len(Q_TYPE_LABLE_DICT)),
+                  'answer_text': answer,
+                  'answer_type_label': F.one_hot(torch.tensor([[ANS_TYPE_LABLE_DICT[answer_type]]]),
+                                                 len(ANS_TYPE_LABLE_DICT)),
+                  'qid': qid}
+        # 'image_organ_label': F.one_hot(torch.tensor([[IMG_ORGAN_LABLE_DICT[image_organ]]]), len(IMG_ORGAN_LABLE_DICT))}
 
         x = (sample['image'], sample['question'])
         if self.task == "main_answer":
@@ -142,7 +144,7 @@ class OVQADataset(Dataset):
             assert False
 
         return x, y
-    
+
 
 def ovqa_dataloader(batch_size, num_workers, size=228):
     '''
@@ -159,13 +161,13 @@ def ovqa_dataloader(batch_size, num_workers, size=228):
             shuffle = True if phase == 'train' else False
             drop_last = True if phase == 'train' else False
             ovqa_dataset = OVQADataset(json_file=f'{QVQA_PATH}{phase}set.json',
-                                    root_dir=f'{QVQA_PATH}img/', task=d, phase=phase, transform=transform)
-            
-            data_loader[d][phase] = DataLoader(ovqa_dataset, 
-                                              num_workers=num_workers, 
-                                              pin_memory=False,  # Maybe False
-                                              batch_size=batch_size, 
-                                              shuffle=shuffle,
-                                              drop_last=drop_last)
+                                       root_dir=f'{QVQA_PATH}img/', task=d, phase=phase, transform=transform)
+
+            data_loader[d][phase] = DataLoader(ovqa_dataset,
+                                               num_workers=num_workers,
+                                               pin_memory=False,  # Maybe False
+                                               batch_size=batch_size,
+                                               shuffle=shuffle,
+                                               drop_last=drop_last)
             iter_data_loader[d][phase] = iter(data_loader[d][phase])
     return data_loader, iter_data_loader
